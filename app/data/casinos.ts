7785fa07c03e04605c6d8997a85f4296904a2295
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-07 04:45:09 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -31,13 +31,6 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 // Raw casino data without id, rating, and badge
 const casinosRawData: CasinoRawData[] = [
   {
-    name: "LuckyWave",
-    logo: "/luckywave.svg",
-    bonus: "100% UP TO £2,000  + 200 FREE SPINS",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=luckywave",
-    isMobile: true
-  },
-  {
     name: "HarryCasino",
     logo: "/harry.svg",
     bonus: "£1000 BONUS + 100 Free Spins",
@@ -51,12 +44,18 @@ const casinosRawData: CasinoRawData[] = [
     url: "https://topbritcasinos.com/B7W2b6jr?utm_target=qbet",
     isMobile: true
   },
-
   {
     name: "SpinShark",
     logo: "/spinshark.svg",
     bonus: "£900 BONUS + 150 Free Spins",
     url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
+    isMobile: true
+  },
+  {
+    name: "LuckyWave",
+    logo: "/luckywave.svg",
+    bonus: "100% UP TO £2,000  + 200 FREE SPINS",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=luckywave",
     isMobile: true
   },
   {
