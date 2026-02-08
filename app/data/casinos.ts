e35fe4d0880e485f8a92aa39bc0802db434aaff0
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-09 01:43:58 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -74,6 +74,14 @@ const casinosRawData: CasinoRawData[] = [
     votes: 2149,
     isMobile:true
   },
+  {
+    name: "HarryCasino",
+    logo: "/harry.svg",
+    bonus: "£1000 BONUS + 100 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
+    votes: 3951,
+    isMobile:true
+  },
   // {
   //   name: "Qbet",
   //   logo: "/qbet.svg",
