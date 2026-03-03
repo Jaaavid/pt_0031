6a183110666ea182fb23b0151d5569a259390eca
Javid Mamadli <javid.mammadli99@gmail.com>
2026-03-04 00:27:07 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -33,14 +33,6 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 // Raw casino data without id, rating, and badgessss
 const casinosRawData: CasinoRawData[] = [
   {
-    name: "SpinShark",
-    logo: "/spinshark.svg",
-    bonus: "£900 BONUS + 150 Free Spins",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
-    votes: 4143,
-    isMobile: true
-  },
-  {
     name: "Basswin",
     logo: "/basswin.svg",
     bonus: "DEPOSIT TODAY - GET UP TO £3000 + 300 FS",
@@ -56,15 +48,6 @@ const casinosRawData: CasinoRawData[] = [
     votes: 4915,
     isMobile: true
   },
-
-  {
-    name: "HarryCasino",
-    logo: "/harry.svg",
-    bonus: "£1000 BONUS + 100 Free Spins",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
-    votes: 3951,
-    isMobile:true
-  },
   {
     name: "Lizaro",
     logo: "/lizaro.png",
@@ -73,6 +56,24 @@ const casinosRawData: CasinoRawData[] = [
     votes: 5872,
     isMobile: true
   },
+  {
+    name: "SpinShark",
+    logo: "/spinshark.svg",
+    bonus: "£900 BONUS + 150 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
+    votes: 4143,
+    isMobile: true
+  },
+  //
+  // {
+  //   name: "HarryCasino",
+  //   logo: "/harry.svg",
+  //   bonus: "£1000 BONUS + 100 Free Spins",
+  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
+  //   votes: 3951,
+  //   isMobile:true
+  // },
+
 
   // {
   //   name: "Verywell",
