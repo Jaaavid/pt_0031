a5f9406805ae041ffa19b0b6d76e6fa53c8acde4
Javid Mamadli <javid.mammadli99@gmail.com>
2026-03-16 18:02:00 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -33,6 +33,23 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 // Raw casino data without id, rating, and badges
 const casinosRawData: CasinoRawData[] = [
   {
+    name: "OdinFortune",
+    logo: "/odinfortune.png",
+    bonus: "Up to €4,000 + 700 FS",
+    url: "https://topbritcasinos.com/P8qHrrKS?utm_target=odinfortune",
+    votes: 5872,
+    isMobile: true
+  },
+
+  {
+    name: "Basswin",
+    logo: "/basswin.svg",
+    bonus: "DEPOSIT TODAY - GET UP TO £3000 + 300 FS",
+    url: "https://topbritcasinos.com/P8qHrrKS?utm_target=basswin",
+    votes: 3951,
+    isMobile:true
+  },
+  {
     name: "ReveryPlay",
     logo: "/reveryplay.svg",
     bonus: "Up to €5,000 + 250 FS",
@@ -48,23 +65,14 @@ const casinosRawData: CasinoRawData[] = [
     votes: 4915,
     isMobile: true
   },
-  {
-    name: "OdinFortune",
-    logo: "/odinfortune.png",
-    bonus: "Up to €4,000 + 700 FS",
-    url: "https://topbritcasinos.com/P8qHrrKS?utm_target=odinfortune",
-    votes: 5872,
-    isMobile: true
-  },
-
-  {
-    name: "Lizaro",
-    logo: "/lizaro.png",
-    bonus: "200% up to £1000 + 250 FREE SPINS",
-    url: "https://topbritcasinos.com/P8qHrrKS?utm_target=lizaro",
-    votes: 5872,
-    isMobile: true
-  },
+  // {
+  //   name: "Lizaro",
+  //   logo: "/lizaro.png",
+  //   bonus: "200% up to £1000 + 250 FREE SPINS",
+  //   url: "https://topbritcasinos.com/P8qHrrKS?utm_target=lizaro",
+  //   votes: 5872,
+  //   isMobile: true
+  // },
 
   //
   // {
