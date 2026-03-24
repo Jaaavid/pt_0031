11c011e470a51ccca4c951ef5c13cf9184eeeeca
Javid Mamadli <javid.mammadli99@gmail.com>
2026-03-24 22:26:33 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -12,56 +12,56 @@ export interface Casino {
 }
 
 export const casinos: Casino[] = [
-  {
-    name: "SafeCasino",
-    logo: "/safecasino.png",
-    rating: 10,
-    votes: 9547,
-    bonus: "100% até 1000 € + 300 RG + JOGO DE BÓNUS",
-    url: "https://safecasino.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=safecasino",
-    badge: "Melhor Escolha",
-    isMobile: true
-  },
-  {
-    name: "Vegazone",
-    logo: "/vegazone.svg",
-    rating: 9.9,
-    votes: 9547,
-    bonus: "260% UP TO €2,600 + 260 FREE SPINS",
-    url: "https://vegazone.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=vegazone",
-    badge: "Grandes Bónus",
-    isMobile: true
-  },
-  {
-    name: "Aerobet",
-    logo: "/aerobet.webp",
-    rating: 9.7,
-    votes: 9547,
-    bonus: "UP TO 600% + 450 FREE SPINS",
-    url: "https://aerobet.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=aerobet",
-    badge: "Levantamento Instantâneo",
-    isMobile: true
-  },
-  {
-    name: "BetLabel",
-    logo: "/betlabel.svg",
-    rating: 9.8,
-    votes: 9547,
-    bonus: "100% até 300 EUR + 30 FS",
-    url: "https://betlabel.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=betlabel",
-    badge: "Pagamentos Rápidos",
-    isMobile: true
-  },
-  {
-    name: "SilverPlay",
-    logo: "/silverplay.svg",
-    rating: 9.7,
-    votes: 9547,
-    bonus: "100% UP TO 500€ + 50 FREE SPINS",
-    url: "https://silverplay.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=silverplay",
-    badge: "Escolha High Roller",
-    isMobile: true
-  },
+  // {
+  //   name: "SafeCasino",
+  //   logo: "/safecasino.png",
+  //   rating: 10,
+  //   votes: 9547,
+  //   bonus: "100% até 1000 € + 300 RG + JOGO DE BÓNUS",
+  //   url: "https://safecasino.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=safecasino",
+  //   badge: "Melhor Escolha",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "Vegazone",
+  //   logo: "/vegazone.svg",
+  //   rating: 9.9,
+  //   votes: 9547,
+  //   bonus: "260% UP TO €2,600 + 260 FREE SPINS",
+  //   url: "https://vegazone.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=vegazone",
+  //   badge: "Grandes Bónus",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "Aerobet",
+  //   logo: "/aerobet.webp",
+  //   rating: 9.7,
+  //   votes: 9547,
+  //   bonus: "UP TO 600% + 450 FREE SPINS",
+  //   url: "https://aerobet.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=aerobet",
+  //   badge: "Levantamento Instantâneo",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "BetLabel",
+  //   logo: "/betlabel.svg",
+  //   rating: 9.8,
+  //   votes: 9547,
+  //   bonus: "100% até 300 EUR + 30 FS",
+  //   url: "https://betlabel.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=betlabel",
+  //   badge: "Pagamentos Rápidos",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "SilverPlay",
+  //   logo: "/silverplay.svg",
+  //   rating: 9.7,
+  //   votes: 9547,
+  //   bonus: "100% UP TO 500€ + 50 FREE SPINS",
+  //   url: "https://silverplay.sedeciassls.live?sub_id_1=zlaeg1h19lmm22&utm_source=silverplay",
+  //   badge: "Escolha High Roller",
+  //   isMobile: true
+  // },
 
   {
     name: "Bwin",
